5c8b7d40217d9f14f8cfa148da55901f36fe4f6e
s1101137113 <ludan6152@gmail.com>
2015-05-01 19:27:40 +0800

    WEBAPI

---
+++ MvcApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+
+        [HttpGet]
+        [ActionName("byName")]
+        public Employee GetEmployeeByName(string name)
+        {
+            var employee = EmployeeService.GetEmployeeByName(name);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+    }
+}
